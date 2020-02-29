b20d30db096dd83028ba23aa6ff17be44a576285
Hugo Castilho <hugo@castilho.me>
2020-02-29 18:38:42 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     # test_suite='tests',
     # tests_require=test_requirements,
     url='https://github.com/hcastilho/ldsagrader',
-    version='0.1.13',
+    version='0.1.14',
     zip_safe=False,
 )
